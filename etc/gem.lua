ef6f1f63e997e49f6d166c38c685cd871843a3f8
nvs <notveryserious@gmail.com>
2016-05-15 17:19:06 -0700

    etc/gem.lua: Added file
    See `bin/map` for an explanation of the configuration settings.

---
+++ etc/gem.lua
@@ -0,0 +1,13 @@
+return {
+	name = 'gem',
+
+	patch_directory = '../lib/patch/1.27a',
+	output_directory = '../tmp',
+
+	scripts_directory = '../src',
+	scripts = {
+		'gem-3_1'
+	},
+
+	pjass = 'pjass'
+}
