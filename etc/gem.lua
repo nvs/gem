29014018dd13bed77bc25a1dcc705621c38850ac
nvs <notveryserious@gmail.com>
2016-05-17 22:45:38 -0700

    Issue #30: Support Grim Extension Pack
    First off, the extension pack itself is added into the repository.  Like with
    `pjass`, this will make builing the map easier for other people.
    
    Second, the build script and configuration table have been updated to handle
    merging objects into a specified map file.  This is nice as it will allow
    tracking of changes to object data.  Also, it allows the ability to remove
    objects from the map file, and use this process to define them.  Eventually
    all objects could be handled in such a fashion.

--- etc/gem.lua
+++ etc/gem.lua
@@ -1,6 +1,7 @@
 return {
 	name = 'gem',
 
+	map_directory = '../share/base',
 	patch_directory = '../lib/patch/1.27a',
 	output_directory = '../tmp',
 
@@ -12,5 +13,10 @@ return {
 		'config'
 	},
 
+	objects_directory = '../share/objects',
+	objects = {
+	},
+
 	pjass = 'wine ../lib/pjass/pjass.exe',
+	object_merger = 'wine ../lib/grimex/ObjectMerger.exe'
 }
