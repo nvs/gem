56cb1cec049ab59389395852d58158a889ef555b
nvs <notveryserious@gmail.com>
2017-01-22 16:46:04 -0800

    Refactor build script and configuration
    Issues: #261
    
    This is a fairly comprehensive review of the build script. The following list
    sums up the primary changes:
    
    - Globals are now parsed and passed to Lua scripts processed by Grimex (#261).
      This makes it possible to consolidate values in a single location (JASS
      files) and use them elsewhere in the project.
    - Improved handling of external commands and the logging of their outputs.
      Resolving a few inconsistencies in this regard enhances compatibility with
      Lua 5.1 and LuaJIT.
    - Attempt to properly escape command line arguments for both Posix and Windows
      systems. Do realize that this has not been tested on Windows.
    - Project configuration has changed, and is now documented in a separate file.
      Details regarding the handling of globals has been included.
    - Numerous other improvements and bug fixes.

--- docs/building-the-map.md
+++ docs/building-the-map.md
@@ -10,7 +10,7 @@ First we'll discuss how to build the map for release. Over time much of this
 process will become automated, but for now there is still a fair bit of human
 involvement. This step-by-step guide assumes none of the default
 configuration settings have been altered, beyond the needed paths for the
-executables to function properly. 
+executables to function properly.
 
 01. Have a valid Warcraft 3 installation. This is necessary to ensure that
     later tools can properly access the Warcraft 3 MPQ files. You may want to
@@ -23,28 +23,28 @@ executables to function properly.
 03. Clone the repository.
 04. Checkout the version of the map in question. You may have to look at this
     file again as the build process may have changed between versions.
-05. Look at the configuration file (`etc/gem.lua`) and adjust any paths as
+05. Look at the configuration file (`gem.lua`) and adjust any paths as
     necessary. Typically this should not be needed. Of note is the `prefix`
     setting, which is by default set to `'wine'`. Windows users will most
     likely want to set this to an empty string: `''`.
 06. Change into the project's root directory.
 07. Use the build script to initialize a working map file: `bin/map prepare
-    etc/gem.lua`. This will take the base version of the map
+    gem.lua`. This will take the base version of the map
     (`share/base/gem.w3x` in the default case) and place a copy within the
     temporary directory: `tmp/gem.w3x`. This is the file that will be used to
     build the map. Note that the `prepare` command is destructive and will
     overwrite an existing `tmp/gem.w3x` file.
 08. Load constants into the map with the following command: `bin/map constants
-    etc/gem.lua`. This uses GrimEx (which is included in the repository).
+    gem.lua`. This uses GrimEx (which is included in the repository).
 09. Load objects into the map with the following command: `bin/map objects
-    etc/gem.lua`.
-10. Build the map's `war3map.j` file: `bin/map build etc/gem.lua`. This will
+    gem.lua`.
+10. Build the map's `war3map.j` file: `bin/map build gem.lua`. This will
     create the map's script file from multiple sources that are spread out for
     organizational purposes. This makes use of pjass, which is included in the
     repository, to check the JASS files.
 11. Import all the files within the `share/imports` directory into the
     `tmp/gem.w3x` map file with the following command: `bin/map imports
-    etc/gem.lua`.
+    gem.lua`.
 12. Using the World Editor, make the changes mentioned in the [Map File
     Changes] (map-file-changes.md) file.
 13. Also using the World Editor, go into the 'Abilities' tab of the Object
@@ -53,7 +53,7 @@ executables to function properly.
     'Melee, Ranged', and then save the map. This is a workaround for a known
     bug that can occur during the build process.
 14. Use Vexorian's Optimizer (which is included in the repository) to process
-    the map. Simply invokve the build script: `bin/map optimize etc/gem.lua`.
+    the map. Simply invokve the build script: `bin/map optimize gem.lua`.
     This will produce both an optimized map file `tmp/gem-optimized.w3x` and
     a script file `tmp/gem-optimized.w3x.j`. Rename the map file as needed to
     match the version being released. This step is only necessary if you wish
--- etc/gem.lua
+++ etc/gem.lua
@@ -1,132 +1,157 @@
 return {
 	name = 'gem',
 
-	map_directory = 'share/base',
-	patch_directory = 'lib/patch/1.27b',
-	output_directory = 'tmp',
-
-	scripts_directory = 'src',
-	scripts = {
-		'array',
-		'string',
-		'color',
-		'error',
-
-		'id',
-		'table',
-		'handle',
-
-		'spawn',
-		'spawn-group',
-
-		'gem',
-		'player-color',
-		'character-width',
-
-		'unit.event',
-		'unit.event.enters',
-		'unit.event.leaves',
-		'unit.event.death',
-		'unit.event.initialize',
-
-		'unit.indexer.stack',
-		'unit.indexer',
-
-		'dummy-caster',
-
-		'unit.user-data',
-		'unit.disarm',
-		'unit.stun',
-
-		'gem-3_1.globals',
-
-		'gem.spawn',
-
-		'gem-3_1',
-		'gem-3_1.buying-lives',
-
-		'settings.difficulty',
-		'settings.common',
-
-		'time',
-		'board',
-
-		'quests.common',
-
-		'quests.information.what-is-gem-td-plus',
-		'quests.information.special-thanks',
-		'quests.information.random-information',
-
-		'quests.changelogs.1_4',
-		'quests.changelogs.1_3_1',
-		'quests.changelogs.1_3',
-		'quests.changelogs.1_2',
-		'quests.changelogs.1_1',
-		'quests.changelogs.1_0',
-
-		'quests.initialize',
-
-		'settings.setup',
-		'settings.hcl',
-		'settings.window',
-		'settings.initialize',
-
-		'commands.debug',
-		'commands.zoom',
-		'commands',
-
-		'detect-placed-gem-death',
-
-		'gem.slate.hold',
-		'gem.slate.ancient',
-		'gem.slate.damage',
-		'gem.slate.poison',
-		'gem.slate.initialize',
-
-		'gem.special.dark-emerald',
-		'gem.special.jade',
-		'gem.special.initialize',
-
-		'main',
-		'config'
+	input = {
+		map = 'share/maps/gem.w3x'
 	},
 
-	constants_directory = 'share/constants',
-	constants = {
-		gameplay = {
-			'gem'
-		},
+	output = {
+		directory = 'tmp'
+	},
 
-		interface = {
-			'gem',
-			'upkeep'
+	patch = {
+		directory = 'lib/patch/1.27b'
+	},
+
+	scripts = {
+		directory = 'src',
+		files = {
+			'array.j',
+			'string.j',
+			'color.j',
+			'error.j',
+
+			'id.j',
+			'table.j',
+			'handle.j',
+
+			'spawn.j',
+			'spawn-group.j',
+
+			'gem.j',
+			'player-color.j',
+			'character-width.j',
+
+			'unit/event.j',
+			'unit/event/enters.j',
+			'unit/event/leaves.j',
+			'unit/event/death.j',
+			'unit/event/initialize.j',
+
+			'unit/indexer/stack.j',
+			'unit/indexer.j',
+
+			'dummy-caster.j',
+
+			'unit/user-data.j',
+			'unit/disarm.j',
+			'unit/stun.j',
+
+			'gem-3.1/globals.j',
+
+			'gem/spawn.j',
+
+			'gem-3.1.j',
+			'gem-3.1/buying-lives.j',
+
+			'settings/difficulty.j',
+			'settings/common.j',
+
+			'time.j',
+			'board.j',
+
+			'quests/common.j',
+
+			'quests/information/what-is-gem-td-plus.j',
+			'quests/information/special-thanks.j',
+			'quests/information/random-information.j',
+
+			'quests/changelogs/1.4.j',
+			'quests/changelogs/1.3.1.j',
+			'quests/changelogs/1.3.j',
+			'quests/changelogs/1.2.j',
+			'quests/changelogs/1.1.j',
+			'quests/changelogs/1.0.j',
+
+			'quests/initialize.j',
+
+			'settings/setup.j',
+			'settings/hcl.j',
+			'settings/window.j',
+			'settings/initialize.j',
+
+			'commands/debug.j',
+			'commands/zoom.j',
+			'commands.j',
+
+			'detect-placed-gem-death.j',
+
+			'gem/slate/hold.j',
+			'gem/slate/ancient.j',
+			'gem/slate/damage.j',
+			'gem/slate/poison.j',
+			'gem/slate/initialize.j',
+
+			'gem/special/dark-emerald.j',
+			'gem/special/jade.j',
+			'gem/special/initialize.j',
+
+			'main.j',
+			'config.j'
 		}
 	},
 
-	objects_directory = 'share/objects',
-	objects = {
-		'unit-event',
-		'update-map-information',
-		'fix-lumber-costs',
-		'make-slates-repairable',
-		'dummy-caster',
-		'unit-disarm',
-		'unit-stun',
-		'kill-bonus-remove-brilliance-aura',
-		'kill-bonus-remove-spell-immunity',
-		'gold-mine-buttons',
-		'slate-teleport'
+	imports = {
+		directory = 'share/imports'
 	},
 
-	imports_directory = 'share/imports',
+	objects = {
+		directory = 'share/objects',
+		files = {
+			'unit-event.lua',
+			'update-map-information.lua',
+			'fix-lumber-costs.lua',
+			'make-slates-repairable.lua',
+			'dummy-caster.lua',
+			'unit-disarm.lua',
+			'unit-stun.lua',
+			'kill-bonus-remove-brilliance-aura.lua',
+			'kill-bonus-remove-spell-immunity.lua',
+			'gold-mine-buttons.lua',
+			'slate-teleport.lua'
+		},
+	},
+
+	constants = {
+		gameplay = {
+			directory = 'share/constants/gameplay',
+			files = {
+				'gem.lua'
+			}
+		},
+
+		interface = {
+			directory = 'share/constants/interface',
+			files = {
+				'gem.lua',
+				'upkeep.lua'
+			}
+		}
+	},
 
 	prefix = 'wine',
 
-	pjass = 'lib/pjass/pjass.exe',
-	pjass_options = '-rb +shadow +filter -nosyntaxerror -nosemanticerror',
+	pjass = {
+		options = {
+			'-rb',
+			'+shadow',
+			'+filter',
+			'-nosyntaxerror',
+			'-nosemanticerror'
+		}
+	},
 
-	grimex = 'lib/grimex',
-
-	optimizer = 'lib/optimizer/VXJWTSOPT.exe',
-	optimizer_tweaks = 'etc/gem.vxtweak'
+	optimizer = {
+		tweaks = 'etc/gem.vxtweak',
+	}
 }
