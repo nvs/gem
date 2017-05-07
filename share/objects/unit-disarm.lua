cfcf1902cee19ddd852e43861e0861430c0dbfb3
nvs <notveryserious@gmail.com>
2017-05-06 22:37:02 -0700

    src/unit/disarm: Switch to using Cargo Hold
    Issues: #203, #220
    
    This addreses two issues:
    
    - First, the use of Cargo Hold instead of Drunken Haze allows auras and
      buffs to remain on the disarmed unit.
    - Second, the switch from using Drunken Haze avoids an object generation
      bug that could cause disarm to fail if one was not careful when
      building the map.

--- share/objects/unit-disarm.lua
+++ share/objects/unit-disarm.lua
@@ -1,24 +1,23 @@
 -- # Unit Disarm
 --
--- A number of systems use Drunken Haze: UnitStatus; Wurst; etc. Inspiration
--- was drawn from all of them.
+-- This system makes use of Cargo Hold (`'Abun'`) to disable a unit's attack.
 
 setobjecttype ('buffs')
 
 -- ## Disarm Buff
-if objectexists ('BNdh') then
-	createobject ('BNdh', 'UDDB')
+if objectexists ('Basl') then
+	createobject ('Basl', 'UDDB')
 
 	if currentobject () == 'UDDB' then
 		-- Art:
 		makechange (current, 'fart', -- Icon
-		'ReplaceableTextures\\CommandButtons\\BTNBattleStations.blp')
+			'ReplaceableTextures\\CommandButtons\\BTNBattleStations.blp')
 		makechange (current, 'ftat', '') -- Target
 		makechange (current, 'fta0', '') -- Target Attachment Point 1
 
 		-- Text:
 		makechange (current, 'fnsf', '(Unit Disarm)') -- Editor Suffix
-		makechange (current, 'fnam', 'Stun Disarm') -- Name (Editor Only)
+		makechange (current, 'fnam', 'Disarm Buff') -- Name (Editor Only)
 		makechange (current, 'ftip', 'Disarmed') -- Tooltip
 		makechange (current, 'fube', -- Tooltip - Extended
 			'This unit is disarmed: it cannot attack.')
@@ -27,52 +26,37 @@ end
 
 setobjecttype ('abilities')
 
--- ## Disarm Ability
-if objectexists ('ANdh') then
-	createobject ('ANdh', 'UDDA')
+-- ## Disarm Temporary
+if objectexists ('Aasl') then
+	createobject ('Aasl', 'UDDT')
 
-	if currentobject () == 'UDDA' then
-		-- Art:
-		makechange (current, 'amac', 0.00) -- Missile Arc
-		makechange (current, 'amat', '') -- Missile Art
-		makechange (current, 'amsp', 0) -- Missile Speed
+	if currentobject () == 'UDDT' then
 
 		-- Data/Stats:
 		makechange (current, 'aare', 1, 0.00) -- Area of Effect
 		makechange (current, 'abuf', 1, 'UDDB') -- Buffs
+		makechange (current, 'Slo1', 1, 0.00) -- Movement Speed Factor
+		makechange (current, 'atar', 1, 'self') -- Targets Allowed
 
-		-- There is a good chance that this part of the ability will be bugged.
-		-- The easiest workaround is to open the map in the World Editor, find
-		-- the ability, set the value to 'None' then save. Then set the value
-		-- back to 'Melee, Ranged', and then save the map again. It should now
-		-- work properly.
-		makechange (current, 'Nsi1', 1, 3) -- Attacks Prevented (Melee, Ranged)
+		-- Text:
+		makechange (current, 'ansf', '(Unit Disarm)') -- Editor Suffix
+		makechange (current, 'anam', 'Disarm Temporary') -- Name
+	end
+end
 
-		makechange (current, 'Nsi2', 1, 0.00) -- Chance To Miss
-		makechange (current, 'Nsi3', 1, 0.00) -- Movement Speed Modifier
+-- ## Disarm Ability
+if objectexists ('Abun') then
+	createobject ('Abun', 'UDDA')
 
-		-- So Nestharus claims this is the maximum range. No clue if this is
-		-- true, but let's go with it. The alternative being the diagnol of the
-		-- maximum map size (480 x 480).
-		makechange (current, 'aran', 1, 92083) -- Cast Range
-		makechange (current, 'acdn', 1, 0.00) -- Cooldown
-		makechange (current, 'Htb1', 1, 0) -- Damage
-		makechange (current, 'adur', 1, 0.00) -- Duration (Normal)
-		makechange (current, 'ahdu', 1, 0.00) -- Duration (Hero)
-		makechange (current, 'alev', 1) -- Levels
-		makechange (current, 'amcs', 1, 0) -- Mana Cost
-		makechange (current, 'arac', 0) -- Race
+	if currentobject () == 'UDDA' then
 
-		-- This is needed to be able to stun units with spell immunity.
-		makechange (current, 'arlv', 6) -- Required Level
-
-		-- This is needed to stun all types of units.
-		makechange (current, 'atar', 1, -- Targets Allowed
-			'invulnerable,vulnerable')
+		-- Data/Stats:
+		makechange (current, 'Car1', 1, 0) -- Cargo Capacity
 
 		-- Text:
 		makechange (current, 'ansf', '(Unit Disarm)') -- Editor Suffix
 		makechange (current, 'anam', 'Disarm Ability') -- Name
-		makechange (current, 'aord', 'drunkenhaze') -- Order String - Use/Turn On
 	end
 end
+
+--
