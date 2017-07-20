a1d918be1a1743e6d11c7e9dca466bf08264a05d
nvs <notveryserious@gmail.com>
2017-07-19 23:04:42 -0700

    share/constants/interace: Refactor Upkeep script

--- share/constants/interface/upkeep.lua
+++ share/constants/interface/upkeep.lua
@@ -11,23 +11,38 @@ local forum = color_link (globals.Gem__WEBSITE_FORUM.value)
 local discord = color_link (globals.Gem__WEBSITE_DISCORD.value)
 local repository = color_link (globals.Gem__WEBSITE_REPOSITORY.value)
 
+local white = '|cff' .. globals.Color__WHITE.value
+
+local function color_white (text)
+	return white .. text .. '|r'
+end
+
+local function color_red (text)
+	return '|cff' .. globals.Color__RED.value .. text .. '|r'
+end
+
+local maintainer = color_red (globals.Gem__MAINTAINER.value)
+
+local function color_gold (text)
+	return '|cff' .. globals.Color__GOLD.value .. text .. '|r'
+end
+
 -- Text - General 'No Upkeep'
-setvalue ('FrameDef', 'UPKEEP_NONE', '|cfffed312' .. name_version .. '|r')
+setvalue ('FrameDef', 'UPKEEP_NONE', color_gold (name_version))
 
 -- Text - General 'Gold Income Rate:'
-setvalue ('FrameDef', 'COLON_GOLD_INCOME_RATE', string.format (
-	'|cffffffffMaintained by |r|cffff0000%s|r|cffffffff —',
-	globals.Gem__MAINTAINER.value))
+setvalue ('FrameDef', 'COLON_GOLD_INCOME_RATE',
+	color_white ('Maintained by ') .. maintainer .. white .. ' —')
 
 -- Text - General - 'Upkeep is determined...'
 setvalue ('FrameDef', 'RESOURCE_UBERTIP_UPKEEP',
-	'Forum — ' .. forum .. '|n' ..
-	'Discord — ' .. discord .. '|n' ..
-	'Repository — ' .. repository)
+	'- ' .. forum .. '|n' ..
+	'- ' .. discord .. '|n' ..
+	'- ' .. repository)
 
 -- Text - General - '|n%d-%d Food...'
 --
 -- An empty string does not work here. This ensures that nothing is displayed
 -- by this field, which typically lists food ranges and associated income
 -- values.
-setvalue ('FrameDef', 'RESOURCE_UBERTIP_UPKEEP_INFO', '|cffffffff|r')
+setvalue ('FrameDef', 'RESOURCE_UBERTIP_UPKEEP_INFO', '|r')
