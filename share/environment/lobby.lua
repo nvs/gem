d7d05b817ae7ddd271d28a59c3e55f5d05387ba2
nvs <notveryserious@gmail.com>
2019-04-03 22:01:57 -0700

    [Environment/Lobby] Fix red's player name
    Issue: #407
    
    Red's player name has been `TRIGSTR_001` due to a missing trigger string
    definition since the original Bryvx line of the map.  This ensures that
    the default name for all players its `Player #`.

--- share/environment/lobby.lua
+++ share/environment/lobby.lua
@@ -4,3 +4,8 @@ local map = ...
 map.information.forces [1].name = string.format ('%s by %s',
 	map.globals.Gem__NAME, map.globals.Gem__MAINTAINER)
 map.information.forces [2].name = map.globals.Gem__WEBSITE_DISCORD
+
+-- Ensure that default player names are `Player #`.
+for index, player in ipairs (map.information.players) do
+	player.name = string.format ('Player %d', index)
+end
