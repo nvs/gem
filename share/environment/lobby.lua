158080dd509fe7e6e81cf6786819a1ebb28370ff
nvs <notveryserious@gmail.com>
2019-04-03 22:01:42 -0700

    [Environment/Lobby] Add comment

--- share/environment/lobby.lua
+++ share/environment/lobby.lua
@@ -1,5 +1,6 @@
 local map = ...
 
+-- Set force names to be more informative.
 map.information.forces [1].name = string.format ('%s by %s',
 	map.globals.Gem__NAME, map.globals.Gem__MAINTAINER)
 map.information.forces [2].name = map.globals.Gem__WEBSITE_DISCORD
