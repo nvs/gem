79a7767214022d2670e5fb8dab8db33340f81854
nvs <notveryserious@gmail.com>
2019-07-25 22:48:58 -0700

    Lib/Gem: Add deep copy function

---
+++ lib/gem/deep_copy.lua
@@ -0,0 +1,21 @@
+local function deep_copy (old, ignore_metatable)
+	local new
+
+	if type (old) == 'table' then
+		new = {}
+
+		for key, value in pairs (old) do
+			new [deep_copy (key)] = deep_copy (value)
+		end
+
+		if not ignore_metatable then
+			setmetatable (new, deep_copy (getmetatable (old)))
+		end
+	else
+		new = old
+	end
+
+	return new
+end
+
+return deep_copy
