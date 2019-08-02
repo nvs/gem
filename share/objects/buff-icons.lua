a92f956629afb8f30e2d32070e95a09a7467bc56
nvs <notveryserious@gmail.com>
2019-08-02 15:42:43 -0700

    Buffs: Fix icons without borders

---
+++ share/objects/buff-icons.lua
@@ -0,0 +1,9 @@
+local map = ...
+local objects = map.objects
+
+objects.B002.fart.value =
+	'ReplaceableTextures\\CommandButtons\\BTNFeedBack.blp'
+objects.B00A.fart.value =
+	'ReplaceableTextures\\CommandButtons\\BTNReplenishHealth.blp'
+objects.B00J.fart.value =
+	'ReplaceableTextures\\CommandButtons\\BTNMagicImmunity.blp'
