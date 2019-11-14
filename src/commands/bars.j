nvs/gem

commit 4ce87f14ba3bd5443e84c88de5b12cd020e339b5

Upgrades: Fix unit scaling on cancel/finish
Issue: #508

src/commands/bars.j
@@ -106,16 +106,10 @@ function Commands___Bars_Upgrade takes nothing returns boolean
 	local player whom = GetOwningPlayer (which)
 	local integer index = GetPlayerId (whom)
 
-	// TODO: This should be in its own area.  But it isn't.  This should
-	// address unit models being the wrong size in replays after upgrading.
-	if what == EVENT_PLAYER_UNIT_UPGRADE_FINISH then
-		set scale = BlzGetUnitRealField (which, UNIT_RF_SCALING_VALUE)
-		call SetUnitScale (which, scale, scale, scale)
-
 	// The upgrade needs to be started over again to allow the unit to have
 	// a positive selection scale from the beginning.  This ensures that the
 	// selection circle will stay.
-	elseif scale < 0 then
+	if scale < 0 then
 		call DisableTrigger (GetTriggeringTrigger ())
 		call Commands___Display_Bar (which, true)
 

src/fix/upgrade-scale.j
@@ -1,0 +1,24 @@
+// # Upgrade Scale
+//
+// When a unit upgrade is finished or cancelled the scale of the unit can be
+// wrong.  This keeps that behavior from presenting.
+
+function Fix_Upgrade_Scale___Event takes nothing returns boolean
+	local unit which = GetTriggerUnit ()
+	local real scale = BlzGetUnitRealField (which, UNIT_RF_SCALING_VALUE)
+
+	call SetUnitScale (which, scale, scale, scale)
+
+	return true
+endfunction
+
+function Fix_Upgrade_Scale__Initialize takes nothing returns boolean
+	local trigger upgrade
+
+	set upgrade = Trigger__New ()
+	call Trigger__Try (upgrade, function Fix_Upgrade_Scale___Event)
+	call TriggerRegisterAnyUnitEventBJ (upgrade, EVENT_PLAYER_UNIT_UPGRADE_CANCEL)
+	call TriggerRegisterAnyUnitEventBJ (upgrade, EVENT_PLAYER_UNIT_UPGRADE_FINISH)
+
+	return false
+endfunction

src/main.j
@@ -182,6 +182,7 @@ function main takes nothing returns nothing
 	call TriggerAddCondition (initialize, Condition (function Unit_User_Data__Initialize))
 	call TriggerAddCondition (initialize, Condition (function Unit_Stun__Initialize))
 	call TriggerAddCondition (initialize, Condition (function Unit_Disarm__Initialize))
+	call TriggerAddCondition (initialize, Condition (function Fix_Upgrade_Scale__Initialize))
 
 	call TriggerAddCondition (initialize, Condition (function Gem_Information__Initialize))
 

commit 93baa357e8b71b3ebd0321d1b5b9f3018b25ffa6

Bars: Fix chain upgrading
Issue: #515

If a player had enough gold, a tower would upgrade multiple times.  This
only applied to Silver, Malachite, and Star Ruby.  This bug was
introduced in 4ce87f14ba3bd5443e84c88de5b12cd020e339b5.  When behavior
was split out of the file an associated event was not disabled.

src/commands/bars.j
@@ -158,5 +158,4 @@ function Commands___Initialize_Bars takes nothing returns nothing
 	call Trigger__Try (upgrade, function Commands___Bars_Upgrade)
 	call TriggerRegisterAnyUnitEventBJ (upgrade, EVENT_PLAYER_UNIT_UPGRADE_CANCEL)
 	call TriggerRegisterAnyUnitEventBJ (upgrade, EVENT_PLAYER_UNIT_UPGRADE_START)
-	call TriggerRegisterAnyUnitEventBJ (upgrade, EVENT_PLAYER_UNIT_UPGRADE_FINISH)
 endfunction